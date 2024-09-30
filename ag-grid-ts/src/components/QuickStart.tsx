69f71ebe425b3345420ef70879da8f4996e165c1
Derek <derek@smallboat.com>
2024-09-30 14:46:51 +0800

    enable edit

--- ag-grid-ts/src/components/QuickStart.tsx
+++ ag-grid-ts/src/components/QuickStart.tsx
@@ -10,6 +10,7 @@ import { ColDef, ModuleRegistry } from "@ag-grid-community/core";
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 
 interface IRow {
+  id: number;
   make: string;
   model: string;
   price: number;
@@ -20,24 +21,26 @@ ModuleRegistry.register(ClientSideRowModelModule);
 const QuickStart = () => {
   // Row Data: The data to be displayed.
   const [rowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
-    { make: "Fiat", model: "500", price: 15774, electric: false },
-    { make: "Nissan", model: "Juke", price: 20675, electric: false },
+    { id: 1, make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { id: 2, make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { id: 3, make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { id: 4, make: "Mercedes", model: "EQA", price: 48890, electric: true },
+    { id: 5, make: "Fiat", model: "500", price: 15774, electric: false },
+    { id: 6, make: "Nissan", model: "Juke", price: 20675, electric: false },
   ]);
 
   // Column Definitions: Defines & controls grid columns.
   const [colDefs] = useState<ColDef<IRow>[]>([
-    { field: "make", headerName: "Maker" },
-    { field: "model", headerName: "Model" },
-    { field: "price", headerName: "$Price" },
-    { field: "electric", headerName: "Electric" },
+    { field: "id", headerName: "Id", cellDataType: "number", editable: false },
+    { field: "make", headerName: "Maker", cellDataType: "string" },
+    { field: "model", headerName: "Model", cellDataType: "string" },
+    { field: "price", headerName: "$Price", cellDataType: "number" },
+    { field: "electric", headerName: "Electric", cellDataType: "boolean" },
   ]);
 
   const defaultColDef = {
     flex: 1,
+    editable: true,
   };
 
   // Container: Defines the grid's theme & dimensions.
@@ -47,7 +50,9 @@ const QuickStart = () => {
       <AgGridReact
         rowData={rowData}
         columnDefs={colDefs}
+        readOnlyEdit={false}
         defaultColDef={defaultColDef}
+
       />
     </div>
   );
